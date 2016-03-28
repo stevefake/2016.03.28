3bb86b50508c4f120b21817eb5d676e7467111e3
Steve Fake <steven.m.fake@gmail.com>
2016-03-28 13:26:33 -0400

    initial commit

---
+++ estimates.rb
@@ -0,0 +1,30 @@
+
+# write down a number of hours that you expect each part to take (1-3). Record your hours as you go.
+# A GitHub repository.
+# A quality README.
+# A reflection on your estimate. Well? How did you do?
+# A shell script. Of a curl command to close an issue.
+# Primary Task
+# Your task tonight is to create a Ruby application which will query multiple API
+# endpoints from Github, display a list of current issues, and prompt the user to close one of these issues.
+## 2 hours
+
+# You will need to create at least two issues on your repo by hand.
+## 1 hour
+
+# Make sure that your display is easy to read for users. A pile of numbers and
+# words with no descriptors won't do anyone any good.
+## 20 minutes
+
+# Your code must be written in classes and be spread across multiple files. It
+# is suggested that you build one class for each API endpoint which you access,
+# and then you create instances of each class when you have parameters to make a
+# call to that endpoint. If you can think of a better way that still involves classes, though, go for it.
+## 1 hour
+
+# Testing is going to be tricky on this one. You should mock the API responses
+# so that you don't need to hit the API every time you run your tests. You should
+# also be able to explain WHY this is good.
+## 1 hour 
+
+# No database is needed for your primary task.
