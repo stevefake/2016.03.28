6acd44d976238f965d1d1c7eb730b4fbfdf3b32d
Steve Fake <steven.m.fake@gmail.com>
2016-03-28 14:17:15 -0400

    update estimates.rb

--- estimates.rb
+++ estimates.rb
@@ -1,9 +1,17 @@
 
 # write down a number of hours that you expect each part to take (1-3). Record your hours as you go.
 # A GitHub repository.
+##Actual time: 10 minutes
+
 # A quality README.
+##Actual time: 10 minutes
+
 # A reflection on your estimate. Well? How did you do?
+##
+
 # A shell script. Of a curl command to close an issue.
+## 2 hours
+
 # Primary Task
 # Your task tonight is to create a Ruby application which will query multiple API
 # endpoints from Github, display a list of current issues, and prompt the user to close one of these issues.
@@ -11,6 +19,7 @@
 
 # You will need to create at least two issues on your repo by hand.
 ## 1 hour
+### Actual 5 minutes
 
 # Make sure that your display is easy to read for users. A pile of numbers and
 # words with no descriptors won't do anyone any good.
@@ -25,6 +34,6 @@
 # Testing is going to be tricky on this one. You should mock the API responses
 # so that you don't need to hit the API every time you run your tests. You should
 # also be able to explain WHY this is good.
-## 1 hour 
+## 1 hour
 
 # No database is needed for your primary task.
